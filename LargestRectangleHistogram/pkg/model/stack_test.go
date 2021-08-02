880e38703fa7658042ff34e76b3df241b301b21a
Saffat Hasan <saffathasan@gmail.com>
2021-08-01 21:06:54 -0400

    feat(LargestRectangleHistogram): add test for full coverage

--- LargestRectangleHistogram/pkg/model/stack_test.go
+++ LargestRectangleHistogram/pkg/model/stack_test.go
@@ -21,6 +21,35 @@ func TestStack(t *testing.T) {
 	assert.Equal(t, val, 1)
 
 	val, err = s.Pop()
-	assert.NotNil(t, err)
+	assert.Equal(t, err.Error(), "tried to pop from empty stack")
 	assert.Equal(t, 0, val)
+}
+
+func TestStackLen(t *testing.T) {
+	s := Stack{}
+
+	assert.Equal(t, s.Len(), 0)
+	s.Push(1)
+	assert.Equal(t, s.Len(), 1)
+	s.Push(1)
+	assert.Equal(t, s.Len(), 2)
+	s.Pop()
+	assert.Equal(t, s.Len(), 1)
+	s.Pop()
+	assert.Equal(t, s.Len(), 0)
+	s.Pop()
+	assert.Equal(t, s.Len(), 0)
+}
+
+func TestStackPeek(t *testing.T) {
+	s := Stack{}
+
+	s.Push(0)
+	assert.Equal(t, s.Peek(), 0)
+
+	s.Push(1)
+	assert.Equal(t, s.Peek(), 1)
+
+	s.Pop()
+	assert.Equal(t, s.Peek(), 0)
 }
